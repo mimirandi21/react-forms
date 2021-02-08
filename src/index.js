bef24114ab68f756ee876c351d3c523f1c28694a
Randi Schreiner <mimirandi21@gmail.com>
2021-02-08 14:55:26 -0600

    commit tree a3c61036351115981df71076aa7f53353aff2497

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ParentComponent from "./components/ParentComponent";
 
 ReactDOM.render(
   <div>
-    <Form />
+    <ParentComponent />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
